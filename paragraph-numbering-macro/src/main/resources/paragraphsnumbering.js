33b4fdc061358d3458d33bff36727c3fc810469f
Manuel Leduc <manuel.leduc@xwiki.com>
2021-12-06 15:01:04 +0100

    PNAPP-15: Introduce a specific behavior on enter on the paragraphs

--- paragraph-numbering-macro/src/main/resources/paragraphsnumbering.js
+++ paragraph-numbering-macro/src/main/resources/paragraphsnumbering.js
@@ -17,8 +17,68 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-require(['deferred!ckeditor'], function (ckeditorPromise) {
-  ckeditorPromise.done(ckeditor => {
-    
-  })
-});
+require(['jquery', 'deferred!ckeditor',], function ($, ckeditorPromise) {
+    function activated(editor) {
+      let ascendant = editor.getSelection().getStartElement()
+        .getAscendant(element => {
+          if (!element.getAttribute) {
+            return false;
+          }
+          let attribute = element.getAttribute("class");
+          let split = attribute?.split(' ');
+          return split?.includes('paragraphs-numbering-root')
+        }, true);
+      return ascendant !== null;
+    }
+
+    ckeditorPromise.done(ckeditor => {
+      if (!('xwiki-numberedParagraphs' in ckeditor.plugins.registered)) {
+        ckeditor.plugins.add('xwiki-numberedParagraphs', {
+          init: function (editor) {
+            console.log('INITIALIZING')
+            handleEnterKey(editor)
+          }
+        });
+
+        ckeditor.on('instanceCreated', event => {
+          if (event.editor.config.extraPlugins === '') {
+            event.editor.config.extraPlugins = 'xwiki-numberedParagraphs';
+          } else {
+            event.editor.config.extraPlugins += ',xwiki-numberedParagraphs';
+          }
+        });
+      }
+
+      //
+      // Enter specific plugin.
+      //
+      const handleEnterKey = function (editor) {
+        editor.on('afterCommandExec', event => {
+          if (event.data.name === 'enter') {
+            afterEnterKey(editor);
+          }
+        });
+      };
+
+      const afterEnterKey = function (editor) {
+        const selection = editor.getSelection();
+        // We intercept the Enter key only if the selection is collapsed, and numbered content is activated.
+        if (selection.isCollapsed() && activated(editor)) {
+          const parentBlock = selection.getStartElement()
+            .getAscendant(element => ckeditor.dtd.$block[element.getName()], true);
+          const previousBlock = parentBlock.getPrevious();
+          if (parentBlock.is('p') && /h[1-6]/.test(previousBlock.getName())) {
+            if (previousBlock.getText().trim().length) {
+              const bookmarks = selection.createBookmarks2();
+              parentBlock.renameNode(previousBlock.getName());
+              selection.selectBookmarks(bookmarks);
+            } else {
+              previousBlock.remove();
+            }
+          }
+        }
+      };
+    });
+
+  }
+)
